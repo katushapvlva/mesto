1c1663bfecdb69999c3eb9c4efe73975e8849833
Katusha Pavlova <katepav2001@mail.ru>
2022-09-15 13:17:58 +0300

    js - теперь можно менять имя и о себе в профиле

--- script.js
+++ script.js
@@ -2,10 +2,24 @@ let openPopup = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closePopup = popup.querySelector('.popup__close');
 
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__form-name'); 
+let jobInput = formElement.querySelector('.popup__form-description'); 
+
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__description');
+
 function popupToggle() {
   popup.classList.toggle('popup_opened');
-  console.log('Нажала кнопку')
+}
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
 }
 
 openPopup.addEventListener('click', popupToggle);
 closePopup.addEventListener('click', popupToggle);
+formElement.addEventListener('submit', formSubmitHandler); 
