a70c59898fccc29e529a147cc069df8138b861ed
Katusha Pavlova <katepav2001@mail.ru>
2022-10-01 15:39:44 +0300

    Сделала заготовку для окна добавления места

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,23 @@
-let openPopup = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closePopup = popup.querySelector('.popup__close');
+let openPopupEdit = document.querySelector('.profile__edit-button');
+let openPopupAdd = document.querySelector('.profile__add-button'); // new
+let popupEdit = document.querySelector('#popup__edit'); // new
+let popupAdd = document.querySelector('#popup__add'); // new
+let closePopupEdit = document.querySelector('#popup__close-edit'); // new
+let closePopupAdd = document.querySelector('#popup__close-add'); // new
 
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__input-text_type_name'); 
 let jobInput = formElement.querySelector('.popup__input-text_type_description'); 
+let placeInput = formElement.querySelector('.popup__input-text_type_place'); // new
+let linkInput = formElement.querySelector('.popup__input-text_type_link'); // new
 
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__description');
+let placeTitle = document.querySelector('.elements__title'); // new
+let linkPhoto = document.querySelector('.elements__photo'); // new
 
-function popupToggle() {
-  popup.classList.toggle('popup_opened');
+function popupToggle(popup) { // new
+  popup.classList.toggle('popup_opened'); // new
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
@@ -21,9 +28,22 @@ function formSubmitHandler (evt) {
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
 
-  popupToggle();
+  // placeTitle.textContent = placeInput.value; // new
+  // linkPhoto.textContent = linkInput.value; // new
+
+  popupToggle(popupEdit); // "заглушка"
 }
 
-openPopup.addEventListener('click', popupToggle);
-closePopup.addEventListener('click', popupToggle);
+openPopupEdit.addEventListener('click', () => {
+  popupToggle(popupEdit);
+}); // new
+openPopupAdd.addEventListener('click', () => {
+  popupToggle(popupAdd);
+}); // new
+closePopupEdit.addEventListener('click', () => {
+  popupToggle(popupEdit);
+}); // new
+closePopupAdd.addEventListener('click', () => {
+  popupToggle(popupAdd);
+}); // new
 formElement.addEventListener('submit', formSubmitHandler);
